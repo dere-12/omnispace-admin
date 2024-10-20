d7f66707fac8d8ae1c6df48ca34b24cb1da87bee
Dereje <dereje2012k@gmail.com>
2024-10-20 09:46:09 +0300

    add isAdmin functionality

--- api/controllers/authen.controller.js
+++ api/controllers/authen.controller.js
@@ -53,7 +53,10 @@ export const signin = async (req, res, next) => {
     }
 
     // if email and password are correct we authenticate a user using jwt. package
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
     const { password: pass, ...rest } = validUser._doc;
 
